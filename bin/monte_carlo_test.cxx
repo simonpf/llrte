61a5d977209d38f12f61c8630701586f674b04ef
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2019-10-15 15:44:19 +0200

    Added missing CMakeLists file.

---
+++ bin/monte_carlo_test.cxx
@@ -0,0 +1,11 @@
+#include <llrte/compiler/backends.h>
+#include <llrte/compiler.h>
+#include <llrte/grids.h>
+
+int main(int /*argc*/, const char **/***argv*/) {
+
+
+    auto grid = llrte::RegularGrid<float>();
+
+
+}
