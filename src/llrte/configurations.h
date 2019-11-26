6a63f5b578fd22d51cb75735812bc2a86e4f9930
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2019-11-26 13:07:19 +0100

    Added missing configurations.h

---
+++ src/llrte/configurations.h
@@ -0,0 +1,20 @@
+#ifndef _LLRTE_CONFIGURATIONS_H_
+#define _LLRTE_CONFIGURATIONS_H_
+
+namespace llrte {
+
+  template<typename F>
+  struct Limits;
+
+  template<>
+  struct Limits<float> {
+    static constexpr float eps = 1e-3;
+  };
+
+  template<>
+  struct Limits<double> {
+    static constexpr double eps = 1e-6;
+  };
+} // namspace llrte
+#endif
+
